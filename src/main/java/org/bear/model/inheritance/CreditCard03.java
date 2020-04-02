e5ef3049f67829c2d71b890d308fe67ae6ed96d3
bear_i <rnmdvd@gmail.com>
2020-04-02 19:34:36 +0300

    Joined tables inheritance strategy example

---
+++ src/main/java/org/bear/model/inheritance/CreditCard03.java
@@ -0,0 +1,81 @@
+package org.bear.model.inheritance;
+
+//Joined-tables inheritance strategy example
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@PrimaryKeyJoinColumn(name = "credit_card_id")
+public class CreditCard03 extends BillingDetails03 {
+
+	@NotNull
+	@Column(name = "card_number")
+	private String cardNumber;
+
+	@NotNull
+	@Column(name = "exp_month")
+	private String expMonth;
+
+	@NotNull
+	@Column(name = "exp_year")
+	private String expYear;
+
+	public String getCardNumber() {
+		return cardNumber;
+	}
+
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+
+	public String getExpMonth() {
+		return expMonth;
+	}
+
+	public void setExpMonth(String expMonth) {
+		this.expMonth = expMonth;
+	}
+
+	public String getExpYear() {
+		return expYear;
+	}
+
+	public void setExpYear(String expYear) {
+		this.expYear = expYear;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if(this == o) return true;
+		if(o == null || getClass() != o.getClass()) return false;
+		if(!super.equals(o)) return false;
+
+		CreditCard02 that = (CreditCard02) o;
+
+		if(getCardNumber() != null ? !getCardNumber().equals(that.getCardNumber()) : that.getCardNumber() != null) return false;
+		if(getExpMonth() != null ? !getExpMonth().equals(that.getExpMonth()) : that.getExpMonth() != null) return false;
+		return getExpYear() != null ? getExpYear().equals(that.getExpYear()) : that.getExpYear() == null;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = super.hashCode();
+		result = 31 * result + (getCardNumber() != null ? getCardNumber().hashCode() : 0);
+		result = 31 * result + (getExpMonth() != null ? getExpMonth().hashCode() : 0);
+		result = 31 * result + (getExpYear() != null ? getExpYear().hashCode() : 0);
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "CreditCard02{" +
+				"cardNumber='" + cardNumber + '\'' +
+				", expMonth='" + expMonth + '\'' +
+				", expYear='" + expYear + '\'' +
+				'}';
+	}
+}
+
