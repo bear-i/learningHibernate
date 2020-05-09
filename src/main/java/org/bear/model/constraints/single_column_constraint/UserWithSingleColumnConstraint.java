5953943dad5fcc914a9e65552d3d4aa1a3fc0b68
bear_i <rnmdvd@gmail.com>
2020-05-09 21:21:12 +0300

    single column constraint

---
+++ src/main/java/org/bear/model/constraints/single_column_constraint/UserWithSingleColumnConstraint.java
@@ -0,0 +1,72 @@
+package org.bear.model.constraints.single_column_constraint;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(catalog = "sample", schema = "public", name = "user_with_single_column_constraint")
+public class UserWithSingleColumnConstraint {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(
+			name = "username",
+			columnDefinition = "varchar(15) not null unique" + " check(not substring(lower(username), 0, 5) = 'admin')"
+	)
+	private String username;
+
+	public UserWithSingleColumnConstraint() {
+	}
+
+	public UserWithSingleColumnConstraint(String username) {
+		this.username = username;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if(this == o) return true;
+		if(o == null || getClass() != o.getClass()) return false;
+
+		UserWithSingleColumnConstraint that = (UserWithSingleColumnConstraint) o;
+
+		if(getId() != null ? !getId().equals(that.getId()) : that.getId() != null) return false;
+		return getUsername() != null ? getUsername().equals(that.getUsername()) : that.getUsername() == null;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = getId() != null ? getId().hashCode() : 0;
+		result = 31 * result + (getUsername() != null ? getUsername().hashCode() : 0);
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "UserWithSingleColumnConstraint{" +
+				"id=" + id +
+				", username='" + username + '\'' +
+				'}';
+	}
+}
